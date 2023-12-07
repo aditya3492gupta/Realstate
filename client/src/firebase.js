faa11dcc6e51629c3d622eac2c60949374dfeaa0
Aditya Gupta <aditya3492gupta@gmail.com>
2023-12-07 22:57:59 +0530

    project added

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsH4rclF0leqH0Sg5ffBjUW8Ny1GF7XxA",
+  authDomain: "realstate1-7ad0e.firebaseapp.com",
+  projectId: "realstate1-7ad0e",
+  storageBucket: "realstate1-7ad0e.appspot.com",
+  messagingSenderId: "801958159827",
+  appId: "1:801958159827:web:e293113c2408d13888bb26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
